2f0cbe4cd84644dbc52e000e5c8b17f902156b25
Michael Jagiello <michaeljr623@hotmail.com>
2025-04-11 20:32:31 -0400

    implemented playing youtube audio

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ Just a bot of random things I add to it
 
 Ensure you have python3 interpreter installed
 
+It is also a requirement to have FFmpeg installed
+
+This includes as an environment path variable (or in this directory) on Windows, or installing on Linux
+
 1. ``git clone LINK``
 2. ``cd discord-bot``
 3. ``pip install -r ./requirements.txt``
--- bot.py
+++ bot.py
@@ -1,12 +1,28 @@
+import asyncio
 from discord.ext import commands
 import discord
 from dotenv import load_dotenv
 import os
 import yt_dlp
 
+try:
+    import nacl
+except ImportError:
+    try:
+        if os.name == "nt":
+            os.system("py -m pip install pynacl")
+        else:
+            os.system("python3 -m install pynacl")
+    except Exception as e:
+        print("PyNaCl install error: ", e)
+        exit()
+
 intents=discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents) # , help_command=None
 intents.message_content = True
+
+# (url, title)
+linkQueue = []
 
 
 # commands
@@ -22,15 +38,22 @@ async def clear(ctx):
 @bot.command()
 async def join(ctx):
     if ctx.author.voice:
-        await ctx.send("join")
+        await ctx.author.voice.channel.connect()
+        await ctx.send("joined voice channel")
         return True
     else:
-        await ctx.send("not join")
+        await ctx.send("you gotta be in a voice channel first")
         return False
 
 @bot.command()
 async def leave(ctx):
-    await ctx.send("leave")
+    if ctx.voice_client:
+        await ctx.voice_client.disconnect()
+        await ctx.send("left voice channel")
+        return True
+    else:
+        await ctx.send("didn't leave the voice channel I'm not in")
+        return False
 
 @bot.command()
 async def pause(ctx):
@@ -50,11 +73,31 @@ async def play(ctx, *, query="null"):
     if query == "null":
         await ctx.send("need query")
         return
-    await ctx.send("play " + query)
+    
+    if ("youtube.com" in query) or ("youtu.be" in query):
+        info = getInfo(query)
+    else:
+        await ctx.send(f"searching: {query}")
+        info = searchYoutube(query)
+    
+    if not info:
+        await ctx.send("video not found")
+        return
+    
+    url = info["url"]
+    title = info["title"]
+
+    if not ctx.voice_client.is_playing():
+        await ctx.send(f"playing: {title}")
+        playVideo(ctx.voice_client, url)
+    else:
+        linkQueue.append((url, title))
+        await ctx.send(f"queued: {title}")
 
 @bot.command()
 async def queue(ctx):
-    await ctx.send("queue")
+    for video in linkQueue:
+        await ctx.send(linkQueue[video])
 
 @bot.command()
 async def resume(ctx):
@@ -62,7 +105,8 @@ async def resume(ctx):
 
 @bot.command()
 async def skip(ctx):
-    await ctx.send("skip")
+    await ctx.send("skipping")
+    checkNext(ctx.voice_client)
 
 @bot.command()
 async def stop(ctx):
@@ -75,7 +119,43 @@ async def unpause(ctx):
 
 # helpers
 
+def checkNext(voice_client):
+    if linkQueue:
+        song_url, song_title = linkQueue.pop(0)
+        playVideo(voice_client, song_url)
+        asyncio.run_coroutine_threadsafe(voice_client.channel.send(f"playing: {song_title}"), bot.loop)
 
+def getInfo(url):
+    ydl_opts = {
+        "format": "bestaudio/best",
+        "quiet": True
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        try:
+            info = ydl.extract_info(url, download=False)
+            return { "url": info["url"], "title": info.get("title", "unknown title") }
+        except Exception as e:
+            print(f"Error fetching song info: {e}")
+            return None
+
+def playVideo(voice_client, url):
+    source = discord.FFmpegPCMAudio(url)
+    voice_client.play(source, after=lambda e: checkNext(voice_client))
+
+def searchYoutube(query):
+    ydl_opts = {
+        "default_search": "ytsearch",
+        "format": "bestaudio/best",
+        "noplaylist": True,
+        "quiet": True
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(query, download=False)
+        if "entries" in info and len(info["entries"]) > 0:
+            top = info["entries"][0]
+            return { "url": top["url"], "title": top.get("title", "unknown title") }
+    
+    return None
 
 
 load_dotenv()
